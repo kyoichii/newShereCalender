d25b35a7eb0ad2f4f97cf8be1f83541f1ea14fa1
Kyoichi <kyoichi.807@gmail.com>
2021-10-07 12:20:49 +0900

    プロフ画面：ファイルの作成、次回デザインに取り掛かる

---
+++ newShereCalender/ProfileSetting.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileSetting.swift
+//  newShereCalender
+//
+//  Created by 田野恭一 on 2021/10/07.
+//
+
+import SwiftUI
+
+// MARK: プロフィール設定画面
+struct ProfileSetting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileSetting_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileSetting()
+    }
+}
