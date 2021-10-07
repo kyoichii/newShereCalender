337868e1b091b4e19857a76013793c0f3dcf9a59
Kyoichi <kyoichi.807@gmail.com>
2021-10-07 10:41:18 +0900

    新規登録画面の作成

---
+++ newShereCalender/RegisterView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterView.swift
+//  newShereCalender
+//
+//  Created by 田野恭一 on 2021/10/07.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
