b00f4dab68990b9c2364dfa0f783b1eaef98288b
Kyoichi <kyoichi.807@gmail.com>
2021-10-26 16:24:52 +0900

    予定追加ビューの作成

---
+++ newShereCalender/AddView.swift
@@ -0,0 +1,20 @@
+//
+//  AddView.swift
+//  newShereCalender
+//
+//  Created by 田野恭一 on 2021/10/26.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView()
+    }
+}
