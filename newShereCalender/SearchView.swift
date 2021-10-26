3128acdc5c4a6859806c4c3f409f2b1414bd708a
Kyoichi <kyoichi.807@gmail.com>
2021-10-26 16:24:18 +0900

    検索ビューの作成

---
+++ newShereCalender/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  newShereCalender
+//
+//  Created by 田野恭一 on 2021/10/26.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
