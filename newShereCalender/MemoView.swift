88361078399c596969ff2ca4597705554d45b67c
Kyoichi <kyoichi.807@gmail.com>
2021-10-26 16:25:46 +0900

    メモビューの作成

---
+++ newShereCalender/MemoView.swift
@@ -0,0 +1,20 @@
+//
+//  MemoView.swift
+//  newShereCalender
+//
+//  Created by 田野恭一 on 2021/10/26.
+//
+
+import SwiftUI
+
+struct MemoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MemoView_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoView()
+    }
+}
