e34b6026c24ef6df0d8108782cdb5b494ce2fdb2
Kyoichi <kyoichi.807@gmail.com>
2021-10-26 16:25:34 +0900

    カレンダービューの作成

---
+++ newShereCalender/CalenderView.swift
@@ -0,0 +1,20 @@
+//
+//  CalenderView.swift
+//  newShereCalender
+//
+//  Created by 田野恭一 on 2021/10/26.
+//
+
+import SwiftUI
+
+struct CalenderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalenderView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalenderView()
+    }
+}
